8f148aad9430a6af94a5518353a190dab4a27445
Luke Sheppard <sheppiegit@gmail.com>
2023-07-08 17:56:25 -0400

    Added mintNFT function and rteurn token uri function.

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -1,2 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNFT is ERC721 {
+    uint256 private tokenCounter;
+
+    mapping(uint256 => string) private tokenIdToURI;
+
+    constructor() ERC721("Dog", "DGT") {}
+
+    function mintNFT(string memory tokenUri) public {
+        tokenIdToURI[tokenCounter] = tokenUri;
+        _safeMint(msg.sender, tokenCounter);
+        tokenCounter++;
+    }
+
+    function tokenURI(
+        uint256 tokenID
+    ) public view override returns (string memory) {
+        return tokenIdToURI[tokenID];
+    }
+}
