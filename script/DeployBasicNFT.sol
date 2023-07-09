4963f34304e6fa6291ffa1ca65a4015dce4907b4
Luke Sheppard <sheppiegit@gmail.com>
2023-07-09 13:17:36 -0400

    Added a deployment script for the BasicNFT.

--- script/DeployBasicNFT.sol
+++ script/DeployBasicNFT.sol
@@ -1,6 +1,13 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {BasicNFT} from "../src/BasicNFT.sol";
 
-contract DeployBasicNFT {}
+contract DeployBasicNFT is Script {
+    function run() external returns (BasicNFT) {
+        vm.startBroadcast();
+        BasicNFT basicNFT = new BasicNFT();
+        vm.stopBroadcast();
+        return basicNFT;
+    }
+}
